216361b59861eba940d9b16e407bbd83787ce3dd
Deepak Sharma <deepakshar661@gmail.com>
2026-01-15 22:15:09 +0530

    App Enhancements

--- app/page.js
+++ app/page.js
@@ -1,48 +1,14 @@
 "use client"
 import { useState } from "react";
+import { useRouter } from 'next/navigation';
 export default function Home() {
-  const [room,setRoom] = useState(false);
-  const [name,setName] = useState("");
-  const [room_code,setRoom_code] = useState("");
-  const createroom = async ()=>{
-    const res = await fetch("/api/rooms",{
-      method:'POST',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials:'include',
-        body: JSON.stringify({ name, room_code }),
-    })
-    setRoom(false);
-    setRoom_code("");
-  }
-  const joinroom = async()=>{
-
-  }
+  const router = useRouter();
+  
   return (
     <div>
       THis is the dashboard
-      <button onClick={()=>setRoom(true)}> Create Room  </button>
-      {
-        room &&(
-          <div>
-          <input type="text" placeholder="Name" onChange={(e)=>setName(e.target.value)}/>
-          <input type="text" placeholder="Room Code" onChange={(e)=>setRoom_code(e.target.value)}/>
-          <button onClick={createroom}>Submit</button>
-          </div>
-        )
-      }
-      {
-        !room &&(
-          <div>
-            Join ROom
-            <input type="text" onChange={(e)=>{setRoom_code(e.target.value)}}/>
-            <button onClick={joinroom}> Join Room 
-
-            </button>
-          </div>
-        )
-      }
+     <button onClick={()=> router.push("/create")}>Create Room</button>
+     <button onClick={()=> router.push("/join")}>Join Room</button>
     </div>
   );
 }
