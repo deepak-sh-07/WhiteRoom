02ee22bc739c88602370b669c9c889a0e5a2cbe6
Deepak Sharma <deepakshar661@gmail.com>
2026-01-17 22:40:54 +0530

    App Enhancements

--- app/create/page.js
+++ app/create/page.js
@@ -20,7 +20,6 @@ export default function Home() {
   return (
     <div>
       THis is the dashboard
-      <button > Create Room  </button>
           <div>
           <input type="text" placeholder="Name" onChange={(e)=>setName(e.target.value)}/>
           <input type="text" placeholder="Room Code" onChange={(e)=>setRoom_code(e.target.value)}/>
